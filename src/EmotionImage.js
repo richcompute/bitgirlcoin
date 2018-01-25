7215b5b1910e84d0f38b89131b7b0a5d8e4a3b4e
richcompute <calcul@lecalculriche.com>
2018-01-25 09:16:04 -0800

    image happy/sad depending on bitcoin value

--- src/EmotionImage.js
+++ src/EmotionImage.js
@@ -11,15 +11,37 @@ class EmotionImage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      image: null
     }
-  }
+    }
+
   componentDidMount(){
+      const data = this.props.data;
+      const price = this.props.currentPrice;
+
+      const dayChange = price - data[data.length-1].y;
+      const dayChangeP = (price - data[data.length-1].y) / data[data.length-1].y * 100;
+
+      var imageToSet;
+      if (dayChangeP > 0) {
+        imageToSet = imagehappy;
+      } else if (dayChangeP < 0) {
+        imageToSet = imagesad;
+      } else {
+        imageToSet = imagenormal;
+      }
+
+    this.setState(
+        {
+            image: imageToSet
+        }
+    )
   }
   componentWillUnmount(){
   }
   render(){
     return (
-           <img src={imagehappy} />  
+           <img src={this.state.image} />
     );
   }
 }
